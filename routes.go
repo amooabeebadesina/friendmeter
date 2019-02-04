462a0dbfb347f471c3f55b90ff348bc17864853f
Amoo Abeeb <amooabeeb0@gmail.com>
2019-02-04 11:14:43 +0100

    Update route handlers

--- routes.go
+++ routes.go
@@ -2,33 +2,45 @@ package main
 
 import (
 	"encoding/json"
-	"knowme/controllers"
+	"html/template"
+	"knowme/models"
+	"knowme/utils"
 	"net/http"
 
 	"github.com/go-chi/chi"
 )
 
-var homeController = controllers.Home{}
+var jsonResponse utils.JSONResponse
 
 func indexRoute(w http.ResponseWriter, r *http.Request) {
-	t := homeController.Index()
+	paths := []string{
+		"templates/index.html",
+	}
+	t, _ := template.ParseFiles(paths...)
 	t.Execute(w, nil)
 }
 
 func addQuestions(w http.ResponseWriter, r *http.Request) {
+	var question models.Question
+
 	authorization := r.Header.Get("authorization")
 	if authorization == "" {
-		res, _ := json.Marshal("Get the fuck out nigga")
+		res, _ := json.Marshal("You might have lost your way")
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write(res)
 		return
 	}
-	data := r.Body
-	res := homeController.CreateQuestions(&Db, data)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	data := r.Body
+
+	if err := json.NewDecoder(data).Decode(&question); err != nil {
+		panic(err)
+	}
+
+	question.Create(Db)
+
+	utils.SendSuccessResponse(w, question)
+
 }
 
 func routers() *chi.Mux {
